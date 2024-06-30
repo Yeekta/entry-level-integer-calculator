43dd6232dd1cd54a4c15262210672ba605980f58
Yekta <99590210+Yeekta@users.noreply.github.com>
2024-07-01 00:27:04 +0300

    Add files via upload

---
+++ hesap makinesi/hesap makinesi/hesap makinesi.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string carpma;
+    string bolme;
+    string toplama;
+    string cikarma;
+    string input;
+
+    int a, b;
+
+    cout << "Hesap makinesine hosgeldin ( sadece tam sayilar)" << endl; // calculator for integers only
+    cout << "Islemi Yaziniz" << endl;
+
+    cin >> input;
+    
+    if (input == "carpma") // multiplication
+    {
+        cout << "1. sayiyi sec" << endl;
+        cin >> a;
+        
+        cout << "2.sayiyi sec" << endl;
+        cin >> b;
+
+        cout << a * b;
+    }
+    else if (input == "bolme") // division
+    {
+        cout << "1. sayiyi sec" << endl;
+        cin >> a;
+
+        cout << "2.sayiyi sec" << endl;
+        cin >> b;
+
+        cout << a / b;
+    }
+    else if (input == "toplama") // addition
+    {
+        cout << "1. sayiyi sec" << endl;
+        cin >> a;
+
+        cout << "2.sayiyi sec" << endl;
+        cin >> b;
+
+        cout << a + b;
+    }
+    else if (input == "cikarma") // Subtraction process
+    {
+        cout << "1. sayiyi sec" << endl;
+        cin >> a;
+
+        cout << "2.sayiyi sec" << endl;
+        cin >> b;
+
+        cout << a - b;
+    }
+    else
+    {
+        cout << "Hatali islem sectiniz"; // If the action is selected incorrectly, print it on the screen
+    }
+
+
+
+
+
+}
+
